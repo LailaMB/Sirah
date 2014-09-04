f293a941f93f41dc11c8627ab803a95fdd8cd30c
lyaly000 <lyaly000@hotmail.com>
2014-09-04 13:00:21 +0300

    Progress

--- app/controllers/player.js
+++ app/controllers/player.js
@@ -45,9 +45,51 @@ function stopTrack() {
         { 
             audioPlayer.release();
         }  }
+
+audioPlayer.addEventListener('progress',function(e) 
+{
+  
+     if (Ti.Platform.name === 'android')
+        { 
+   var playedTime=formatTime(Math.round(audioPlayer.time/1000))+" : "+formatTime(Math.round(audioPlayer.duration/1000));
+        } 
+      else 
+		{
+			
+    var playedTime=formatTime(Math.round(audioPlayer.progress/1000))+" : "+formatTime(Math.round(audioPlayer.duration/1000));
+
+		}       
+   
+   $.duration.setText(playedTime);
+   
+   var value=0;
+   if (audioPlayer.time > 0) {
+      value = Math.floor((100 / audioPlayer.duration) * audioPlayer.time);
+   }
+      
+   $.timeProgress.applyProperties({width: value + "%" });
+    
+});
+
+
+
+function formatTime(totalSeconds){   
+    var minutes =  parseInt(totalSeconds / 60 % 60);
+    var seconds = parseInt(totalSeconds % 60);
+    return (minutes > 9 ? minutes : "0" + minutes) + ":" + (seconds > 9 ? seconds : "0" + seconds);
+};
+
+$.timeBar.addEventListener('click',function(e){
+
+    var newTime =(e.x/250)*audioPlayer.duration;
+    audioPlayer.pause();
+    audioPlayer.setTime(newTime);
+    audioPlayer.start();
+    
+ 
+});
         
         
- // function showQuestion() {
- // var questionWin=Alloy.createController('question',q).getView();
- // $.player.open(questionWin);
-//};
+ function showQuestion() {
+ // var questionWin=Alloy.createController('question',q).getView().open();
+};
